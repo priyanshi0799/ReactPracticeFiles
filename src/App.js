b55eb732dc3981d0533a53a8a15ea1e5fff9084f
Priyanshi Gupta <priyanshigupta0708@gmail.com>
2020-08-25 22:15:30 +0530

    Adding classname to div

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   //     <h1>I am React App</h1>
   //   </div>
   // );
-  return React.createElement('div',null,React.createElement('h1',null,'Hi There'))
+  return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi There'))
 }
 
 export default App;
