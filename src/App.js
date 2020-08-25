bef95e13400522f2e359f82a6c2c0e9674309a67
Priyanshi Gupta <priyanshigupta0708@gmail.com>
2020-08-25 22:14:13 +0530

    Working of JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div>
-      <h1>I am React App</h1>
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <h1>I am React App</h1>
+  //   </div>
+  // );
+  return React.createElement('div',null,React.createElement('h1',null,'Hi There'))
 }
 
 export default App;
