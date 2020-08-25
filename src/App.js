7995d8a0ee502e55d351edd337baf737777c3a50
Priyanshi Gupta <priyanshigupta0708@gmail.com>
2020-08-25 22:21:29 +0530

    Added a new Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Person from './Person/Person';
 
 function App() {
-  // return (
-  //   <div>
-  //     <h1>I am React App</h1>
-  //   </div>
-  // );
-  return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi There'))
+  return (
+    <div>
+      <h1>I am React App</h1>
+      <Person />
+    </div>
+  );
+  // return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi There'))
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Person = () => {
+    return (
+        <div>
+            <p>I am a Person</p>
+        </div>
+    )
+}
+
+export default Person;
